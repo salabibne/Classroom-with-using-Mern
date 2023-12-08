78143d5b32f3eb8b4fc2af1b417a452d1908f112
SALAB\dellv <salabyaham@gmail.com>
2023-12-08 23:29:06 +0600

    asignment section has been done

--- src/Course/ContinueCourse.jsx
+++ src/Course/ContinueCourse.jsx
@@ -1,9 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link, useParams } from "react-router-dom";
+import usePublicApi from "../Hooks/usePublicApi";
 
 
 const ContinueCourse = () => {
+    const { id } = useParams()
+    const publicApi = usePublicApi()
+    const { data: classAssignments = [], refetch } = useQuery({
+        queryKey: ["enrollClass"],
+        queryFn: async () => {
+            const res = await publicApi.get(`/student/continueCourse/${id}`)
+            return res.data;
+        }
+    })
     return (
-        <div>
-            <p>Welcome to Hel</p>
+        <div style={{ backgroundColor: 'rgb(34, 51, 59)' }}>
+            <div className="py-14 flex">
+                <div className="w-3/4 border-r-4 border-indigo-500 grid grid-cols-2">
+                    {  
+                        classAssignments.map(ca => <div key={ca._id} className="card w-96 bg-base-100 shadow-xl ml-10 mt-16">
+                            <div className="card-body">
+                                <h2 className="card-title font-bold">{ca.title}</h2>
+                                <h2 className="card-title ">
+                                    <div className="badge badge-info gap-2">
+                                        
+                                       DeadLine : {ca.deadline}
+                                    </div>
+                                </h2>
+
+                          
+
+
+                            </div>
+                        </div>)
+                    }
+
+                </div>
+                <div className="w-1/4">p</div>
+            </div>
         </div>
     );
 };
